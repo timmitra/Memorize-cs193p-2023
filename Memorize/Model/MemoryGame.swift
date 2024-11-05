661f3c3c3a507bced862bb19774b5cfe77ebaf27
Tim Mitra <tim@it-guy.com>
2024-11-05 14:52:42 -0500

    finshing model

--- Memorize/Model/MemoryGame.swift
+++ Memorize/Model/MemoryGame.swift
@@ -15,14 +15,29 @@ struct MemoryGame<CardContent> {
     /// private(set) means others can only "get" cards
     private(set) var cards: Array<Card>
     
+    /// cardContentFactory is a function that returns CardContent
+    /// added pairIndex so now cardContentFactory takes an Int
+    /// let content: CardContent = cardContentFactory(pairIndex) can infer CardContent type.
+    init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent) {
+        // cards = Array<Card>()
+        // cards = [Card]()
+        cards = [] /// swift infers that this is empty array of Card
+        // add numberOfPairsOfCards x 2 cards
+        for pairIndex in 0..<numberOfPairsOfCards {
+            let content = cardContentFactory(pairIndex) // create the cardContent
+            cards.append(Card(content: content))
+            cards.append(Card(content: content))
+        }
+    }
+    
     func choose(_ card: Card) {
         
     }
     
     /// nested struct is really MemorizeGame.Card
     struct Card {
-        var isFaceUp: Bool
-        var isMatched: Bool
-        var content: CardContent
+        var isFaceUp = false
+        var isMatched = false
+        let content: CardContent
     }
 }

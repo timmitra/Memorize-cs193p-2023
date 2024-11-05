586e061a96e11f4379eeb47549583a0578ea4a9e
Tim Mitra <tim@it-guy.com>
2024-11-05 14:52:16 -0500

    viewModel done, using static vars and funcs

--- Memorize/ViewModel/EmojiMemoryGame.swift
+++ Memorize/ViewModel/EmojiMemoryGame.swift
@@ -11,12 +11,24 @@
 
 import SwiftUI
 
-func createCardContent(forPairAtIndex index: Int) -> String {
-    return ["👻", "🎃", "🤖", "👽", "👾", "😈", "💀", "☠️", "😱", "💩", "🍭", "🧙‍♀️"][index]
-}
-
 class EmojiMemoryGame {
-    private var model = MemoryGame(numberOfPairsOfCards: 4, cardContentFactory: createCardContent)
+    /// Cannot use instance member 'emojis' within property initializer; property initializers run before 'self' is available.
+    /// Make it static so that it's initialized first as a global, it's now "EmojiMemoryGame.emojis", and make it private so not global outside.
+    private static let emojis = ["👻", "🎃", "🤖", "👽", "👾", "😈", "💀", "☠️", "😱", "💩", "🍭", "🧙‍♀️"]
+    
+    /// make this func static so it's also init'd first.
+    /// Unexpected non-void return value in void function
+    /// add return type MemoryGame<String> - cannot infer return types
+    /// "emojis" can be used here in static func without the long name "EmojiMemoryGame.emojis"
+    /// private so only we can create a memo ry game
+    private static func createMemoryGame() -> MemoryGame<String> {
+        return MemoryGame(numberOfPairsOfCards: 4) { pairIndex in
+            return emojis[pairIndex]
+        }
+    }
+    
+    /// don't need "EmojiMemoryGame.createMemoryGame" since it's inside a global func
+    private var model = createMemoryGame()
     
     var cards: Array<MemoryGame <String>.Card> {
         return model.cards

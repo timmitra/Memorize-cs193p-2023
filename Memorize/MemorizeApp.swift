b80689d96a1361f390652c1d0ecd4e9c1473b54e
Tim Mitra <tim@it-guy.com>
2024-11-05 13:15:55 -0500

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,21 @@
+//
+// ---------------------------- //
+// Original Project: Memorize
+// Created on 2024-11-05 by Tim Mitra
+// Mastodon: @timmitra@mastodon.social
+// Twitter/X: timmitra@twitter.com
+// Web site: https://www.it-guy.com
+// ---------------------------- //
+// Copyright © 2024 iT Guy Technologies. All rights reserved.
+
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

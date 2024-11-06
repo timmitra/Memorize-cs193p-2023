eec5c91e3266c976665fdfdc0719a9350993742e
Tim Mitra <tim@it-guy.com>
2024-11-05 23:59:38 -0500

    add the logic to compare the one card we tap with the next. if matched mark them

--- Memorize/Model/MemoryGame.swift
+++ Memorize/Model/MemoryGame.swift
@@ -31,12 +31,28 @@ struct MemoryGame<CardContent> where CardContent: Equatable {
         }
     }
     
+    var indexOfTheOneAndOnlyFaceUpCard: Int?
+    
     mutating func choose(_ card: Card) {
        // print("chose that \(card)")
         /// can't just toggle, card is an argument
         /// Cannot use mutating member on immutable value: 'card' is a 'let' constant
         if let chosenIndex = cards.firstIndex(where: { $0.id == card.id}) {
-            cards[chosenIndex].isFaceUp.toggle()
+            if !cards[chosenIndex].isFaceUp && !cards[chosenIndex].isMatched {
+                if let potentialMatchIndex = indexOfTheOneAndOnlyFaceUpCard {
+                    if cards[potentialMatchIndex].content == cards[chosenIndex].content {
+                        cards[chosenIndex].isMatched = true
+                        cards[potentialMatchIndex].isMatched = true
+                    }
+                    indexOfTheOneAndOnlyFaceUpCard = nil
+                } else {
+                    for index in cards.indices {
+                        cards[index].isFaceUp = false
+                    }
+                    indexOfTheOneAndOnlyFaceUpCard = chosenIndex
+                }
+                cards[chosenIndex].isFaceUp = true
+            }
         }
     }
         
@@ -49,7 +65,7 @@ struct MemoryGame<CardContent> where CardContent: Equatable {
     /// nested struct is really MemorizeGame.Card
     struct Card: Equatable, Identifiable, CustomDebugStringConvertible {
         
-        var isFaceUp = true
+        var isFaceUp = false
         var isMatched = false
         let content: CardContent
         var id: String

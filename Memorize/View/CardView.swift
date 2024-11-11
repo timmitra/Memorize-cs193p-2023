fa3406ae6bfcc53e4ce6229085b7cd51e8e1919b
Tim Mitra <tim@it-guy.com>
2024-11-11 00:21:50 -0500

    use Pie with angle 240

--- Memorize/View/CardView.swift
+++ Memorize/View/CardView.swift
@@ -26,7 +26,7 @@ struct CardView: View {
             Group {
                 base.fill(.white)
                 base.strokeBorder(lineWidth: Constants.lineWidth)
-                Circle()
+                Pie(endAngle: .degrees(240))
                     .opacity(Constants.Pie.opacity)
                     .overlay(
                         Text(card.content)

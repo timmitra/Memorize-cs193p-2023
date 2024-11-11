7f9f4fed918a2c1e4f015e18ddd0eac39f0b6c5a
Tim Mitra <tim@it-guy.com>
2024-11-11 00:14:21 -0500

    add a Pie shape that we can animate

---
+++ Memorize/View/Pie.swift
@@ -0,0 +1,43 @@
+//
+// ---------------------------- //
+// Original Project: Memorize
+// Created on 2024-11-10 by Tim Mitra
+// Mastodon: @timmitra@mastodon.social
+// Twitter/X: timmitra@twitter.com
+// Web site: https://www.it-guy.com
+// ---------------------------- //
+// Copyright © 2024 iT Guy Technologies. All rights reserved.
+
+
+import SwiftUI
+import CoreGraphics
+
+struct Pie: Shape {
+    /// in iOS +y is down
+    var startAngle: Angle = .zero
+    let endAngle: Angle
+    var clockwise = true
+    
+    func path(in rect: CGRect) -> Path {
+        let center = CGPoint(x: rect.midX, y: rect.midY)
+        let radius = min(rect.width, rect.height) / 2
+        let start = CGPoint(
+            x: center.x + radius * cos(startAngle.radians),
+            y: center.y + radius * sin(startAngle.radians)
+        )
+        
+        var p = Path()
+        p.move(to: center)
+        p.addLine(to: start)
+        p.addArc(
+            center: center,
+            radius: radius,
+            startAngle: startAngle,
+            endAngle: endAngle,
+            clockwise: clockwise
+        )
+        p.addLine(to: center)
+        
+        return p
+    }
+}

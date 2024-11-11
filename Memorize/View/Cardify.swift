5f77e106ed9b0e63340eaa21d9bbdd07288f44fc
Tim Mitra <tim@it-guy.com>
2024-11-11 00:41:02 -0500

    create our own Cardify modifier

---
+++ Memorize/View/Cardify.swift
@@ -0,0 +1,12 @@
+//
+// ---------------------------- //
+// Original Project: Memorize
+// Created on 2024-11-11 by Tim Mitra
+// Mastodon: @timmitra@mastodon.social
+// Twitter/X: timmitra@twitter.com
+// Web site: https://www.it-guy.com
+// ---------------------------- //
+// Copyright © 2024 iT Guy Technologies. All rights reserved.
+
+
+import Foundation

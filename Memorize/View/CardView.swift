74f95261f6ed5d6ac0ff3ecabcae80a33d22335f
Tim Mitra <tim@it-guy.com>
2024-11-12 23:01:49 -0500

    now ad implicit animation with easeInOut duration 2 sec when the pair is matched. Much slower celebration

--- Memorize/View/CardView.swift
+++ Memorize/View/CardView.swift
@@ -31,6 +31,7 @@ struct CardView: View {
                     .aspectRatio(1, contentMode: .fit)
                     .padding(Constants.Pie.inset)
                     .rotationEffect(.degrees(card.isMatched ? 360 : 0))
+                    .animation(.easeInOut(duration: 2), value: card.isMatched)
             )
             .padding(Constants.inset)
             .cardify(isFaceUp: card.isFaceUp)

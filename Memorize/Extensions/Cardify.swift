ab1fb1df282a583d8ce6c7afa3111336b9c42067
Tim Mitra <tim@it-guy.com>
2024-11-12 23:24:04 -0500

    now by using Animatable, with animatableData we can flip the cards and have the icon only appear when the card is at 90° we added an initializer to isFaceUp as well

--- Memorize/Extensions/Cardify.swift
+++ Memorize/Extensions/Cardify.swift
@@ -11,8 +11,20 @@
 
 import SwiftUI
 
-struct Cardify: ViewModifier {
-    let isFaceUp: Bool
+struct Cardify: ViewModifier, Animatable {
+    init(isFaceUp: Bool) {
+        rotation = isFaceUp ? 0 : 180
+    }
+    
+    var isFaceUp: Bool {
+        rotation < 90
+    }
+    
+    var rotation: Double
+    var animatableData: Double {
+        get { rotation }
+        set { rotation = newValue }
+    }
     
     func body(content: Content) -> some View {
         ZStack {
@@ -24,7 +36,7 @@ struct Cardify: ViewModifier {
             base.fill()
                 .opacity(isFaceUp ? 0 : 1)
         }
-        .rotation3DEffect(.degrees(isFaceUp ? 0 : 180), axis: (0, 1, 0))
+        .rotation3DEffect(.degrees(rotation), axis: (0, 1, 0))
     }
     
     private struct Constants {
